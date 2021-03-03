3e9c84b9b9dd235acf0ed1c3d530ea85e30cb60a
Tulir Asokan <tulir@maunium.net>
2021-03-03 20:20:59 +0200

    Move clangwrap variables to .gitlab-ci.yml

--- armv7-clangwrap.sh
+++ armv7-clangwrap.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-set -x
-SDK=iphoneos
-SDK_PATH=`xcrun -sdk $SDK -show-sdk-path`
-export IPHONEOS_DEPLOYMENT_TARGET=7.0
-CLANG=`xcrun -sdk $SDK -find clang`
-
-exec "$CLANG" -arch armv7 -target armv7-apple-ios -stdlib=libc++ -isysroot "$SDK_PATH" "$@"
+exec "$CLANG" -v -arch armv7 -target armv7-apple-ios -stdlib=libc++ -isysroot "$SDK_PATH" "$@"
