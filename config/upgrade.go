33cb794d0dc277e7569f6a7561c5ea490ca93575
Eric Rabil <ericjrabil@gmail.com>
2022-06-24 17:23:38 -0400

    Add missing properties from imessage to config upgrader

--- config/upgrade.go
+++ config/upgrade.go
@@ -36,7 +36,11 @@ func DoUpgrade(helper *up.Helper) {
 
 	helper.Copy(up.Str, "imessage", "platform")
 	helper.Copy(up.Str, "imessage", "imessage_rest_path")
+	helper.Copy(up.List, "imessage", "imessage_rest_args")
+	helper.Copy(up.Bool, "imessage", "log_ipc_payloads")
 	helper.Copy(up.Int, "imessage", "ping_interval_seconds")
+	helper.Copy(up.Bool, "imessage", "chat_merging")
+	helper.Copy(up.Bool, "imessage", "tombstone_old_rooms")
 
 	helper.Copy(up.Str, "bridge", "user")
 	helper.Copy(up.Str, "bridge", "username_template")
