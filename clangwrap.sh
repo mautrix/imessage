723b89959ab48cc2924507b29cb309fbcd9869ab
spotty118 <spotty118@gmail.com>
2023-12-17 20:54:38 -0600

    Update clangwrap.sh

--- clangwrap.sh
+++ clangwrap.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-exec "$CLANG" -v -arch $ARCH -target $ARCH-apple-ios -stdlib=libc++ -isysroot "$SDK_PATH" "$@"
+
+# Check if the required variables are set
+if [ -z "$CLANG" ]; then
+    echo "Error: CLANG variable is not set."
+    exit 1
+fi
+
+if [ -z "$ARCH" ]; then
+    echo "Error: ARCH (architecture) variable is not set."
+    exit 1
+fi
+
+if [ -z "$SDK_PATH" ]; then
+    echo "Error: SDK_PATH variable is not set."
+    exit 1
+fi
+
+# Execute Clang with the provided arguments
+echo "Executing Clang with architecture: $ARCH"
+exec "$CLANG" -v -arch "$ARCH" -target "$ARCH"-apple-ios -stdlib=libc++ -isysroot "$SDK_PATH" "$@"
