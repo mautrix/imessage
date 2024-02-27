62e6e044b322af06c45167486ea02a33bc8ca826
Yoni Revah <jrevah@gmail.com>
2024-02-27 00:43:09 -0500

    un-consolidating

--- imessage/bluebubbles/api.go
+++ imessage/bluebubbles/api.go
@@ -293,6 +293,23 @@ func (bb *blueBubbles) handleNewMessage(rawMessage json.RawMessage) (err error) 
 	default:
 		bb.log.Warn().Msg("Incoming message buffer is full")
 	}
+	if !message.ReadAt.IsZero() && data.DateRetracted == 0 && data.DateEdited == 0 {
+		//bb.handleChatReadStatusChanged(rawMessage)
+		senderGUID := data.Chats[0].GUID
+		var receipt = imessage.ReadReceipt{
+			SenderGUID:     senderGUID, // TODO: Make sure this is the right field?
+			IsFromMe:       false,      // changing this to false as I believe read receipts will always be from others
+			ChatGUID:       message.ChatGUID,
+			ReadUpTo:       message.GUID,
+			ReadAt:         message.ReadAt,
+			JSONUnixReadAt: message.JSONUnixReadAt,
+		}
+		select {
+		case bb.receiptChan <- &receipt:
+		default:
+			bb.log.Warn().Msg("Incoming message buffer is full")
+		}
+	}
 	return nil
 }
 
@@ -323,6 +340,24 @@ func (bb *blueBubbles) handleMessageUpdated(rawMessage json.RawMessage) (err err
 	default:
 		bb.log.Warn().Msg("Incoming message buffer is full")
 	}
+	if !message.ReadAt.IsZero() && data.DateRetracted == 0 && data.DateEdited == 0 {
+		//bb.handleChatReadStatusChanged(rawMessage)
+		senderGUID := data.Chats[0].GUID
+		var receipt = imessage.ReadReceipt{
+			SenderGUID:     senderGUID, // TODO: Make sure this is the right field?
+			IsFromMe:       false,      // changing this to false as I believe read receipts will always be from others
+			ChatGUID:       message.ChatGUID,
+			ReadUpTo:       message.GUID,
+			ReadAt:         message.ReadAt,
+			JSONUnixReadAt: message.JSONUnixReadAt,
+		}
+		select {
+		case bb.receiptChan <- &receipt:
+		default:
+			bb.log.Warn().Msg("Incoming message buffer is full")
+		}
+	}
+
 	return nil
 }
 
@@ -1555,23 +1590,6 @@ func (bb *blueBubbles) convertBBMessageToiMessage(bbMessage Message) (*imessage.
 			Type:       imessage.TapbackFromName(bbMessage.AssociatedMessageType),
 		}
 		message.Tapback.Parse()
-	} else if !message.ReadAt.IsZero() && bbMessage.DateRetracted == 0 && bbMessage.DateEdited == 0 {
-		//bb.handleChatReadStatusChanged(rawMessage)
-		senderGUID := bbMessage.Chats[0].GUID
-		var receipt = imessage.ReadReceipt{
-			SenderGUID:     senderGUID,
-			IsFromMe:       false,
-			ChatGUID:       message.ChatGUID,
-			ReadUpTo:       message.GUID,
-			ReadAt:         message.ReadAt,
-			JSONUnixReadAt: message.JSONUnixReadAt,
-		}
-		select {
-		case bb.receiptChan <- &receipt:
-		default:
-			bb.log.Warn().Msg("Incoming message buffer is full")
-		}
-
 	} else {
 		message.Tapback = nil
 	}
