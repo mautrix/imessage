741b53e0c511ba4fddcf793802374ec385dc095c
Cameron Aaron <cameronaaron1@gmail.com>
2024-03-30 16:33:47 -0700

    trying to get this merged

--- portal.go
+++ portal.go
@@ -453,12 +453,7 @@ func (portal *Portal) markRead(intent *appservice.IntentAPI, eventID id.EventID,
 		return nil
 	}
 
-	// Only send the read update if it is more recent than the last read time
-	if portal.ReadAt.After(readAt) {
-		return nil
-	} else {
-		portal.ReadAt = readAt
-	}
+	portal.ReadAt = readAt
 
 	var extra CustomReadReceipt
 	if intent == portal.bridge.user.DoublePuppetIntent {
@@ -2143,11 +2138,6 @@ func (portal *Portal) HandleiMessage(msg *imessage.Message) id.EventID {
 	// If the message exists in the database, handle edits or retractions
 	if dbMessage != nil && dbMessage.MXID != "" {
 
-		// Clean up the text so the length is accurate
-		msg.Text = strings.ReplaceAll(msg.Text, "\ufffc", "")
-		msg.Subject = strings.ReplaceAll(msg.Subject, "\ufffc", "")
-
-		// DEVNOTE: It seems sometimes the message is just edited to remove data instead of actually retracting it
 		if msg.IsRetracted ||
 			(len(msg.Attachments) == 0 && len(msg.Text) == 0 && len(msg.Subject) == 0) {
 
@@ -2159,7 +2149,7 @@ func (portal *Portal) HandleiMessage(msg *imessage.Message) id.EventID {
 			}
 
 			overrideSuccess = true
-		} else if msg.IsEdited {
+		} else if msg.IsEdited && dbMessage.Part == 0 {
 
 			// Edit existing message
 			intent := portal.getIntentForMessage(msg, nil)
